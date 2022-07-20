5811232a5e5b0a6edcad4666d3099de8b86a1043
Flin42 <feiyanglin2010@gmail.com>
2022-07-20 18:23:11 -0400

    add templates for rest of cards

---
+++ src/tiles/CardTile.cc
@@ -0,0 +1,18 @@
+#include "tiles/CardTile.h"
+#include <iostream>
+using namespace ftxui;
+using namespace std;
+
+CardTile::CardTile(bool intersection) : Tile(intersection) {};
+
+void CardTile::apply(std::shared_ptr<Player> p) {
+    cout << "landed on card tile" << endl;
+}
+
+Element CardTile::tileElement() {
+    return text(" ") |  bgcolor(Color::OrangeRed1);
+}
+
+Element CardTile::getElement() {
+    return tileElement() | border;
+};
---
+++ src/tiles/HealthTile.cc
@@ -0,0 +1,18 @@
+#include "tiles/HealthTile.h"
+#include <iostream>
+using namespace ftxui;
+using namespace std;
+
+HealthTile::HealthTile(bool intersection) : Tile(intersection) {};
+
+void HealthTile::apply(std::shared_ptr<Player> p) {
+    cout << "landed on health tile" << endl;
+}
+
+Element HealthTile::tileElement() {
+    return text(" ") |  bgcolor(Color::Pink3);
+}
+
+Element HealthTile::getElement() {
+    return tileElement() | border;
+};
