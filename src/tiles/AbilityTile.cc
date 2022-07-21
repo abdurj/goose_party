b8e9c26551060532baaaf28bb9454586d65ddbd8
Fei Lin <47468737+Flin42@users.noreply.github.com>
2022-07-21 10:44:23 -0400

    add templates for rest of cards (#6)

---
+++ src/tiles/AbilityTile.cc
@@ -0,0 +1,18 @@
+#include "tiles/AbilityTile.h"
+#include <iostream>
+using namespace ftxui;
+using namespace std;
+
+AbilityTile::AbilityTile(bool intersection) : Tile(intersection) {};
+
+void AbilityTile::apply(std::shared_ptr<Player> p) {
+    cout << "landed on card tile" << endl;
+}
+
+Element AbilityTile::tileElement() {
+    return text(" ") |  bgcolor(Color::Green);
+}
+
+Element AbilityTile::getElement() {
+    return tileElement() | border;
+};
