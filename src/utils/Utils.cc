41514c583cc731ff1f69c878a87c51cda864149c
SongRui <songks712@gmail.com>
2022-07-21 16:53:21 -0400

    fixed roll to use C++ style random

--- src/utils/Utils.cc
+++ src/utils/Utils.cc
@@ -1,4 +1,3 @@
-#include <cstdlib>
 #include "utils/Utils.h"
 #include <iostream>
 #include <bits/stdc++.h>
@@ -6,18 +5,18 @@
 using namespace std;
 
 namespace utils {
-    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 
-    // TODO: Use C++ style random
     int roll(std::shared_ptr<Player> p) {
-        srand(seed);
-        int roll = rand() % 12;
+        unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+        default_random_engine eng(seed);
+        int roll = eng() % 12;
         cout << "Rolled a: " << roll << endl;
         return roll;
     }
 
     void shufflePlayers(vector<shared_ptr<Player>> &players) {
         // randomize order of the players. 
+        unsigned seed = chrono::system_clock::now().time_since_epoch().count();
         shuffle (players.begin(), players.end(), default_random_engine(seed));
     }
 
