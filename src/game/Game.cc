41f6eeb69446a1ec5b99dd3a9683e063793aef18
SongRe <49730299+SongRe@users.noreply.github.com>
2022-07-21 03:38:54 -0400

    turn cycling added (#7)
    * Add degree/grade tile functionality (#5)
    
    * Player grades getters / setters
    
    * Add degree/grade tile functionality
    
    Co-authored-by: Fei Lin <47468737+Flin42@users.noreply.github.com>* turn cycling added

--- src/game/Game.cc
+++ src/game/Game.cc
@@ -14,30 +14,43 @@ void Game::play(){
     b.print();
     
     vector<shared_ptr<Player>> players;
+    // TODO: Dynamically prompt to choose some number of players on init. Could do this outside of game.play(), maybe game.init()? 
     players.emplace_back(make_shared<Player>(PlayerOptions{PlayerClass::FIGHTER, "Abdur", 1}));
-    b.addPlayer(players[0]);
-
+    players.emplace_back(make_shared<Player>(PlayerOptions{PlayerClass::DEFENDER, "Fei", 2}));
+    players.emplace_back(make_shared<Player>(PlayerOptions{PlayerClass::ROGUE, "sussybaka123", 3}));
+    players.emplace_back(make_shared<Player>(PlayerOptions{PlayerClass::MESSENGER, "messenger", 4}));
+    //b.addPlayer(players[0]);
+    for (auto i : players) {
+        b.addPlayer(i);
+    }
     unsigned int curTurn = 0;
-    char c;
-    while (cin >> c) {
-        cout << "It is Player " << curTurn + 1 << "'s turn." << endl;
-        if (c == 'm') {
-            cout << "Moving Player: " << curTurn + 1 << "." << endl;
-            cout << "Rolling..." << endl;
+    char c = '0';
+    cin.exceptions(ios::eofbit|ios::failbit);
+    while (true) {
+        try {
+            cout << "It is Player " << curTurn + 1 << "'s turn." << endl;
+            cout << "Enter 'm' to roll. Note that this would mark the end of your turn." << endl;
+            cin >> c;
+            if (c == 'm') {
+                cout << "Moving Player: " << curTurn + 1 << "." << endl;
+                cout << "Rolling..." << endl;
 
-            int moves = utils::roll(players[curTurn]);
+                int moves = utils::roll(players[curTurn]);
 
-            b.move(players[curTurn], moves);
+                b.move(players[curTurn], moves);
+                curTurn = (curTurn + 1) % players.size();
+                
+                b.update();
+                b.print();
+            }
 
-            curTurn = (curTurn + 1) % players.size();
-            
-            b.update();
-            b.print();
-        }
-
-        if (c == 'q') {
+            if (c == 'q') {
+                break;
+            }
+        } catch (...) {
             break;
         }
+        
     }
     
 }
