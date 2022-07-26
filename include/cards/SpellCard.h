db4c6bbbd6245361e205429514ca75f026972fef
SongRui <songks712@gmail.com>
2022-07-26 18:04:05 -0400

    removed comments and fixed tests

--- include/cards/SpellCard.h
+++ include/cards/SpellCard.h
@@ -1,28 +1,12 @@
 #ifndef SPELL_CARD_H
 #define SPELL_CARD_H
 #include "cards/Card.h"
-// Player will have a use() method which will call card->apply(Player), but how do we handle cases when a card effect does not need a player?
-// Option 1 - Must always pass in a player. Spell cards that don't need a player will pass in the player calling it. But this would require that we know the type of the card, breaking polymorphism
-// Option 2 - We can distinguish between cards using a function that tells the caller whether it requires a target, and work polymorphically with the spell and trap cards. 
 
 class SpellCard : public Card {
     protected:
     SpellCard();
     public:
-    //virtual void apply(std::shared_ptr<Player>, Board*) = 0;
-    //virtual bool requiresTarget() = 0;
-
 };
-
-
-
-
-
-
-
-
-
-// Other spell cards defined here
 
 
 #endif
--- include/cards/TrapCard.h
+++ include/cards/TrapCard.h
@@ -7,11 +7,7 @@ class TrapCard : public Card {
     TrapCard();
     public:
         virtual void activate(std::shared_ptr<Player>) = 0;
-        //virtual void apply(std::shared_ptr<Player>, Board*) = 0;
-        //virtual bool requiresTarget() = 0;
 };
-
-// Other TRAP cards defined here
 
 
 
